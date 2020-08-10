a2fb3ac0ebb2fd1a221f832657722400ba860f3a
Akshay Mathur <akshay.mathur@sjsu.edu>
2020-08-10 17:30:50 +0530

    add methods to provide Repository and ViewModelFactory to Dagger module

--- app/src/main/java/com/akshay/weatherapp/di/modules/WeatherAppModule.kt
+++ app/src/main/java/com/akshay/weatherapp/di/modules/WeatherAppModule.kt
@@ -1,6 +1,9 @@
 package com.akshay.weatherapp.di.modules
 
+import com.akshay.weatherapp.database.WeatherDatabase
 import com.akshay.weatherapp.network.OpenWeatherApiInterface
+import com.akshay.weatherapp.repositories.DailyForecastRepository
+import com.akshay.weatherapp.ui.weeklyforecast.WeeklyForecastViewModel
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
@@ -11,7 +14,22 @@ class WeatherAppModule {
 
     @Provides
     @Singleton
-    fun providesProgrammingQuotesInterface(retrofit: Retrofit): OpenWeatherApiInterface {
+    fun providesOpenWeatherApiInterface(retrofit: Retrofit): OpenWeatherApiInterface {
         return retrofit.create(OpenWeatherApiInterface::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providesDailyForecastRepository(
+        weatherDatabase: WeatherDatabase,
+        openWeatherApiInterface: OpenWeatherApiInterface
+    ): DailyForecastRepository {
+        return DailyForecastRepository(weatherDatabase, openWeatherApiInterface)
+    }
+
+    @Provides
+    @Singleton
+    fun providesWeeklyForecastViewModelFactory(dailyForecastRepository: DailyForecastRepository): WeeklyForecastViewModel.Factory {
+        return WeeklyForecastViewModel.Factory(dailyForecastRepository)
     }
 }

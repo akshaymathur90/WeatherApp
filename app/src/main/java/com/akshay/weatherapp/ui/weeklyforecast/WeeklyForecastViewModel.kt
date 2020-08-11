ddfffc5e31c8b1321800c880dadff1d075086dd6
Akshay Mathur <akshay.mathur@sjsu.edu>
2020-08-11 15:55:53 +0530

    fixes crash when network connection is not available

--- app/src/main/java/com/akshay/weatherapp/ui/weeklyforecast/WeeklyForecastViewModel.kt
+++ app/src/main/java/com/akshay/weatherapp/ui/weeklyforecast/WeeklyForecastViewModel.kt
@@ -1,17 +1,24 @@
 package com.akshay.weatherapp.ui.weeklyforecast
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.akshay.weatherapp.models.Location
 import com.akshay.weatherapp.repositories.DailyForecastRepository
 import kotlinx.coroutines.launch
+import java.io.IOException
+import java.lang.Exception
 import javax.inject.Inject
 
 class WeeklyForecastViewModel(private val forecastRepository: DailyForecastRepository) :
     ViewModel() {
 
     val dailyForecasts = forecastRepository.forecasts
+
+    companion object {
+        private const val TAG = "WeeklyForecastViewModel"
+    }
 
     class Factory @Inject constructor(var forecastRepository: DailyForecastRepository) :
         ViewModelProvider.Factory {
@@ -22,7 +29,15 @@ class WeeklyForecastViewModel(private val forecastRepository: DailyForecastRepos
 
     fun refreshForecasts(location: Location) {
         viewModelScope.launch {
-            forecastRepository.refreshForecast(location)
+            try {
+                forecastRepository.refreshForecast(location)
+            } catch (networkException: IOException) {
+                //TODO: show error to UI
+                Log.e(TAG, Log.getStackTraceString(networkException))
+            } catch (ex: Exception) {
+                Log.e(TAG, Log.getStackTraceString(ex))
+            }
+
         }
     }
 }
